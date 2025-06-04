4adab4b25d931e0f7a1201f4c1d3ed676abf6beb
Nino <ninodelrieu17@gmail.com>
2025-06-04 16:40:17 +0200

    Initial commit

---
+++ src/main/java/com/bleu/coursesHippiques/CoursesHippiquesApplication.java
@@ -0,0 +1,13 @@
+package com.bleu.coursesHippiques;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesHippiquesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesHippiquesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bleu/coursesHippiques/CoursesHippiquesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bleu.coursesHippiques;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesHippiquesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

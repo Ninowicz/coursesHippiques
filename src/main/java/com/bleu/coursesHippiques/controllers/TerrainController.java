7ac52ebe7496f0604ed92c3d1081131c93c7d2fc
Nino <ninodelrieu17@gmail.com>
2025-06-06 10:31:36 +0200

    TerrainController init bdd

--- src/main/java/com/bleu/coursesHippiques/controllers/TerrainController.java
+++ src/main/java/com/bleu/coursesHippiques/controllers/TerrainController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/courses/hippiques/terrain")
 public class TerrainController {
@@ -21,10 +24,32 @@ public class TerrainController {
 
 
     @PostMapping("ajouterTerrain")
-    public ResponseEntity<Terrain> ajouterTerrain() {
-        Terrain terrain = new Terrain();
-        terrainRepository.save(terrain);
-        System.out.println(terrain.getNomTerrain() + " a ete ajoute. ID : " + terrain.getIdTerrain());
-        return ResponseEntity.ok(terrain);
+    public ResponseEntity<List<Terrain>> creerTerrains() {
+
+        List<Terrain> terrains = new ArrayList<>();
+
+        Terrain t1 = new Terrain("Longchamp", 1200, Terrain.typeTerrain.HERBE);
+        t1.setConditionsAleatoires();
+
+        Terrain t2 = new Terrain("Vincennes", 1600, Terrain.typeTerrain.SABLE);
+        t2.setConditionsAleatoires();
+
+        Terrain t3 = new Terrain("Chantilly", 1000, Terrain.typeTerrain.FIBRE);
+        t3.setConditionsAleatoires();
+
+        Terrain t4 = new Terrain("Deauville", 1400, Terrain.typeTerrain.HERBE);
+        t4.setConditionsAleatoires();
+
+        Terrain t5 = new Terrain("Desert Rouge", 1600, Terrain.typeTerrain.SABLE);
+        t4.setConditionsAleatoires();
+
+        terrains.add(t1);
+        terrains.add(t2);
+        terrains.add(t3);
+        terrains.add(t4);
+
+        terrainRepository.saveAll(terrains);
+
+        return ResponseEntity.ok(terrains);
     }
 }

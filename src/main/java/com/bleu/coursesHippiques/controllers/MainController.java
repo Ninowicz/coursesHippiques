00496cc7058c464fb43e37f0f7f5dd7dd32f70c5
Nino <ninodelrieu17@gmail.com>
2025-06-06 11:09:36 +0200

    MainController + TerrainServices
    --> init base de données dans le maincontroller

--- src/main/java/com/bleu/coursesHippiques/controllers/MainController.java
+++ src/main/java/com/bleu/coursesHippiques/controllers/MainController.java
@@ -1,10 +1,12 @@
 package com.bleu.coursesHippiques.controllers;
 
 import com.bleu.coursesHippiques.beans.Cheval;
+import com.bleu.coursesHippiques.beans.Terrain;
 import com.bleu.coursesHippiques.repositories.ChevalRepository;
 import com.bleu.coursesHippiques.repositories.CourseRepository;
 import com.bleu.coursesHippiques.repositories.TerrainRepository;
 import com.bleu.coursesHippiques.services.ChevalServices;
+import com.bleu.coursesHippiques.services.TerrainServices;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +22,14 @@ public class MainController {
     private final ChevalRepository chevalRepository;
     private final TerrainRepository terrainRepository;
     private final ChevalServices chevalServices;
+    private final TerrainServices terrainsServices;
 
-    public MainController(CourseRepository courseRepository, ChevalRepository chevalRepository, TerrainRepository terrainRepository, ChevalServices chevalServices) {
+    public MainController(CourseRepository courseRepository, ChevalRepository chevalRepository, TerrainRepository terrainRepository, ChevalServices chevalServices, TerrainServices terrainsServices) {
         this.courseRepository = courseRepository;
         this.chevalRepository = chevalRepository;
         this.terrainRepository = terrainRepository;
         this.chevalServices = chevalServices;
+        this.terrainsServices = terrainsServices;
     }
 
     @PostMapping("initBaseDeDonnee")
@@ -34,10 +38,22 @@ public class MainController {
         for (int i = 0; i<15; i++) {
             chevalServices.ajouterCheval();
         }
-//        for (int j = 1; j < 5; j++) {
-//
-//        }
         List<Cheval> chevaux = chevalRepository.findAll();
+
+        terrainsServices.initTerrains();
+        List<Terrain> tousLesTerrains = terrainRepository.findAll();
+        tousLesTerrains.forEach(terrain -> {
+            System.out.println("ID : " + terrain.getIdTerrain());
+            System.out.println("Nom : " + terrain.getNomTerrain());
+            System.out.println("Longueur : " + terrain.getLongueur());
+            System.out.println("Type de terrain : " + terrain.getTypeDeTerrain());
+            System.out.println("Météo : " + terrain.getMeteoEvenement());
+            System.out.println("Taux de blessures : " + terrain.getTauxDeBlessures());
+            System.out.println("-----------------------------------");
+        });
+
+
+
         return ResponseEntity.ok(chevaux);
     }
 }
